9671bb5271156e7f33033cccb63dc606e4c083a5
Xavier Cambar <xcambar@gmail.com>
2013-06-08 01:37:10 +0200

    Adds empty specs for utils and middleware

---
+++ spec/middlewareSpec.js
@@ -0,0 +1,13 @@
+"use strict";
+var chai = require('chai'),
+    sinon = require('sinon');
+chai.use(require('sinon-chai'));
+chai.should();
+
+//@TODO First, test the utils
+xdescribe('middleware', function () {
+  it('should add the user from a valid signature');
+  it('should return an error with an invalid signature');
+  it('should bypass the middleware if no data is available');
+});
+
---
+++ spec/utilsSpec.js
@@ -0,0 +1,10 @@
+"use strict";
+var chai = require('chai'),
+    sinon = require('sinon');
+chai.use(require('sinon-chai'));
+chai.should();
+
+xdescribe('utils', function () {
+  it('should generate a signature from a user id');
+  it('should validate a signature');
+});
