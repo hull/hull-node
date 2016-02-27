5368b09ffdfc0f40f575353b1daf0ca2433f54f3
Romain Dardour <romain@hull.io>
2016-02-27 17:18:29 +0100

    add Readme handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import NotifHandler from './notif-handler';
+import ReadmeHandler from './readme-handler';
 import Hull from './client';
 
 Hull.NotifHandler = NotifHandler;
+Hull.ReadmeHandler = ReadmeHandler;
 
 module.exports = Hull;
