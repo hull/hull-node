6bacb2bf76373b30f1cac14ffeef12e6040bc136
Michal Raczka <michal@raczka.me>
2017-02-23 17:19:45 +0100

    Make the token middleware catch the oauth token

--- src/utils/token-middleware.js
+++ src/utils/token-middleware.js
@@ -5,9 +5,9 @@
  * @param  {Function} next
  */
 export default function tokenMiddleware(req, res, next) {
-  if (req.query && (req.query.hullToken || req.query.token)) {
+  if (req.query && (req.query.hullToken || req.query.token || req.query.state)) {
     req.hull = req.hull || {};
-    req.hull.token = req.query.hullToken || req.query.token;
+    req.hull.token = req.query.hullToken || req.query.token || req.query.state;
   }
   next();
 }
