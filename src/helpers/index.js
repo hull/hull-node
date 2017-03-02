5cc35e6a57214db839aff16c14fe41e258a447ed
Michal Raczka <michal@raczka.me>
2017-03-02 14:43:43 +0100

    Adds updateSettings helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export filterUserSegments from "./filter-user-segments";
 export requestExtract from "./request-extract";
 export setUserSegments from "./set-user-segments";
+export updateSettings from "./update-settings";
