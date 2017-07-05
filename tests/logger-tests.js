1790fb3fbd3a809236a2db3ca6d2d4e016013403
Stephane Bellity <stephane@hull.io>
2017-07-05 12:13:19 +0200

    Add user and account claims to logger context

--- tests/logger-tests.js
+++ tests/logger-tests.js
@@ -30,7 +30,48 @@ describe("Hull Logger", () => {
         foo: "bar"
       }
     });
+  });
 
+
+  it("should add user claims in context", () => {
+    const hull = new Hull({ id: "562123b470df84b740000042", secret: "1234", organization: "test" });
+    hull.asUser({ email: "bob@bob.com", anonymous_id: "123" }).logger.info("test", { foo: "bar" });
+    process.stdout.write = originalWrite;
+    expect(JSON.parse(result)).to.be.eql({
+      context: {
+        id: "562123b470df84b740000042",
+        organization: "test",
+        subject_type: "user",
+        user_email: "bob@bob.com",
+        user_anonymous_id: "123"
+      },
+      level: "info",
+      message: "test",
+      data: {
+        foo: "bar"
+      }
+    });
+  });
+
+  it("should add account claims in context", () => {
+    const hull = new Hull({ id: "562123b470df84b740000042", secret: "1234", organization: "test" });
+    hull.asUser({ email: "bob@bob.com", anonymous_id: "123" }).account({ domain: "bob.com" }).logger.info("test", { foo: "bar" });
+    process.stdout.write = originalWrite;
+    expect(JSON.parse(result)).to.be.eql({
+      context: {
+        id: "562123b470df84b740000042",
+        organization: "test",
+        subject_type: "account",
+        account_domain: "bob.com",
+        user_email: "bob@bob.com",
+        user_anonymous_id: "123"
+      },
+      level: "info",
+      message: "test",
+      data: {
+        foo: "bar"
+      }
+    });
   });
 
   it("should allow passing connectorName to the context and results in connector_name in logs", () => {
