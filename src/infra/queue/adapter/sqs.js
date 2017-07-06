801999bd452e2033426fb0d73792470475172ace
Stephane Bellity <stephane@hull.io>
2017-07-06 19:33:48 +0200

    Experiemental support for SQS queue adapter

---
+++ src/infra/queue/adapter/sqs.js
@@ -0,0 +1,100 @@
+import _ from "lodash";
+import Aws from "aws-sdk";
+import SqsConsumer from "sqs-consumer";
+import Promise from "bluebird";
+
+
+/**
+ * SQS Adapter for queue
+ */
+
+export default class SQSAdapter {
+
+  inactiveCount() {  // eslint-disable-line class-methods-use-this
+    console.warn("Queue adapter inactiveCount not implemented");
+    return Promise.resolve(0);
+  }
+
+  failedCount() {  // eslint-disable-line class-methods-use-this
+    console.warn("Queue adapter failedCount not implemented");
+    return Promise.resolve(0);
+  }
+
+  exit() {  // eslint-disable-line class-methods-use-this
+    return "bye bye";
+  }
+
+  setupUiRouter(router) { // eslint-disable-line class-methods-use-this
+    return router;
+  }
+
+  clean() { // eslint-disable-line class-methods-use-this
+  }
+
+  constructor(options) {
+    this.options = options;
+    Aws.config.update(_.pick(options, "accessKeyId", "secretAccessKey", "region"));
+    this.sqs = new Aws.SQS({ apiVersion: "2012-11-05" });
+    this.sendMessage = Promise.promisify(this.sqs.sendMessage.bind(this.sqs));
+  }
+
+  /**
+   * @param {String} jobName queue name
+   * @param {Object} jobPayload
+   * @return {Promise}
+   */
+  create(jobName, jobPayload = {}, { attempts = 3, delay = 0, priority = 1 } = {}) {
+    return this.sendMessage({
+      MessageDeduplicationId: `${jobName}-${new Date().getTime()}`,
+      MessageGroupId: `${jobName}-${new Date().getTime()}`,
+      DelaySeconds: delay / 1000,
+      MessageAttributes: {
+        jobName: { DataType: "String", StringValue: jobName },
+        attempts: { DataType: "Number", StringValue: attempts.toString() },
+        priority: { DataType: "Number", StringValue: priority.toString() }
+      },
+      MessageBody: JSON.stringify(jobPayload),
+      QueueUrl: this.options.queueUrl
+    });
+  }
+
+  /**
+   * @param {String} jobName
+   * @param {Function -> Promise} jobCallback
+   * @return {Object} this
+   */
+  process(jobName, jobCallback) {
+    const consumer = SqsConsumer.create({
+      messageAttributeNames: [jobName],
+      queueUrl: this.options.queueUrl,
+      sqs: this.sqs,
+      visibilityTimeout: 3600 * 4,
+      terminateVisibilityTimeout: true,
+      handleMessage: (message, done) => {
+        try {
+          const id = message.MessageId;
+          const data = JSON.parse(message.Body);
+          console.warn("Job message:", message);
+          return jobCallback({ id, data })
+          .then(() => done())
+          .catch(done);
+        } catch (err) {
+          return done(err);
+        }
+      }
+    });
+
+    consumer.on("processing_error", (err) => {
+      console.error("queue.adapter.processing_error", err);
+    });
+
+    consumer.on("error", (err) => {
+      console.error("queue.adapter.error", err);
+    });
+
+    consumer.start();
+
+    return this;
+  }
+
+}
