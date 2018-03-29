1160077f2bc59166a81138932a4f238b45ffbf16
Michal Raczka <michal@raczka.me>
2018-03-29 14:41:51 +0200

    Fix event type

--- src/types/hull-event.js
+++ src/types/hull-event.js
@@ -6,8 +6,18 @@
  * @memberof Types
  */
 export type THullEvent = {
-  id: string;
+  event_id: string;
   event: string;
+  event_source: string;
+  event_type: string;
+  track_id: string;
+  user_id: string;
+  anonymous_id: string;
+  session_id: string;
+  ship_id: string;
+  app_id: string;
+  app_name: string;
+  created_at: string;
   context: Object;
   properties: Object;
 };
